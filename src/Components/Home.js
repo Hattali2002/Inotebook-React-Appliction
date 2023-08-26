9c240d43c8621d18a97511a7672899d599a3de5f
Hattali2002 <shivanandhattali2002@gmail.com>
2023-08-26 17:20:22 +0530

    add the details

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
 import Notes from './Notes'
+import { useNavigate } from 'react-router-dom';
 
 export default function Home(props) {
+  const navigate=useNavigate();
+  useEffect(()=>{
+    if(localStorage.getItem('token')===null){
+      navigate("/login");
+    }
+    // eslint-disable-next-line
+  },[]);
   return ( 
     <div className="container">
       <Notes showAlert={props.showAlert} />
--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar, Nav, Container, ButtonGroup, Button } from 'react-bootstrap';
-import { useLocation, useNavigate} from 'react-router-dom'; 
+import { useLocation, useNavigate, Link} from 'react-router-dom'; 
 
 export default function NavbarComp() {
     const token=localStorage.getItem('token'); 
@@ -13,19 +13,19 @@ export default function NavbarComp() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary " bg="dark" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand href="home">Inotebook</Navbar.Brand>
+                <Navbar.Brand as={Link} to="home">Inotebook</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link className={`${location.pathname === "/home" ? "active" : ""}`} href="home">Home</Nav.Link>
-                        <Nav.Link className={`${location.pathname === "/about" ? "active" : ""}`} href="about">About</Nav.Link>
-                        <Nav.Link className={`${location.pathname === "/contact" ? "active" : ""}`} href="contact">Contact</Nav.Link>
+                        <Nav.Link as={Link} className={`${location.pathname === "/home" ? "active" : ""}`} to="home">Home</Nav.Link>
+                        <Nav.Link as={Link} className={`${location.pathname === "/about" ? "active" : ""}`} to="about">About</Nav.Link>
+                        <Nav.Link as={Link} className={`${location.pathname === "/contact" ? "active" : ""}`} to="contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 <ButtonGroup aria-label="Third group" size="sm">
-                    <Button as="a" className={`me-2 ${token!==null?"d-none":""}`} href="login">Login</Button>
-                    <Button as="a" className={`me-2 ${token!==null?"d-none":""}`} href="signup">SignUp</Button>
-                    <Button as="a" className={`me-2 ${token!==null?"":"d-none"}`} href="signup" onClick={onclickHandler}>LogOut</Button>
+                    <Button as={Link} className={`me-2 ${token!==null?"d-none":""}`} to="login">Login</Button>
+                    <Button as={Link} className={`me-2 ${token!==null?"d-none":""}`} to="signup">SignUp</Button>
+                    <Button as={Link} className={`me-2 ${token!==null?"":"d-none"}`} to="signup" onClick={onclickHandler}>LogOut</Button>
                 </ButtonGroup>
             </Container>
         </Navbar>
